04912054e25e7174d7d4b0bb270da53e786dd329
skyitachi <skbysp@gmail.com>
2026-04-21 23:47:27 +0800

    format_string支持std::string

--- include/dann/logger.h
+++ include/dann/logger.h
@@ -178,11 +178,15 @@ void Logger::fatalf(const std::string& format, Args... args) {
     }
 }
 
+inline const char* fmt_arg(const std::string& s) { return s.c_str(); }
+template<typename T>
+inline T&& fmt_arg(T&& t) { return std::forward<T>(t); }
+
 template<typename... Args>
 std::string Logger::format_string(const std::string& format, Args... args) {
-    size_t size = snprintf(nullptr, 0, format.c_str(), args...) + 1;
+    size_t size = snprintf(nullptr, 0, format.c_str(), fmt_arg(args)...) + 1;
     std::unique_ptr<char[]> buf(new char[size]);
-    snprintf(buf.get(), size, format.c_str(), args...);
+    snprintf(buf.get(), size, format.c_str(), fmt_arg(args)...);
     return std::string(buf.get(), buf.get() + size - 1);
 }
 
